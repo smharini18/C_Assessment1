fb7d658613c50155d94c8e4ddc5772621ff7871b
smharini18 <smharini18@gmail.com>
2026-06-02 09:02:29 +0530

    Create C_Problem_2.c

---
+++ C_Problem_2.c
@@ -0,0 +1,11 @@
+// sub 5
+#include <stdio.h>
+
+int main() {
+    int n1,n2;
+    printf("enter the number:");
+    scanf("%d",&n1);
+    n2=n1-5;
+    printf("difference is %d",n2);
+    return 0;
+}
