a9b1b8076ad1260df136b8f9e3c92d4c08ea5539
smharini18 <smharini18@gmail.com>
2026-06-02 09:16:07 +0530

    Create C_Problem_9.c

---
+++ C_Problem_9.c
@@ -0,0 +1,11 @@
+// print huns's digit
+#include <stdio.h>
+
+int main() {
+    int n1,n2;
+    printf("enter the number:");
+    scanf("%d",&n1);
+    n2=n1/100;
+    printf("%d",n2);
+    return 0;
+}
