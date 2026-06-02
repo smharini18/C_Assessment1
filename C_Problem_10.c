8ac93a8bd4f67cc8deae7af2c97de7661fd05cff
smharini18 <smharini18@gmail.com>
2026-06-02 09:17:51 +0530

    Create C_Problem_10.c

---
+++ C_Problem_10.c
@@ -0,0 +1,11 @@
+// print ten's digit of 3
+#include <stdio.h>
+
+int main() {
+    int n1,n2;
+    printf("enter the number:");
+    scanf("%d",&n1);
+    n2=(n1/10)%10;
+    printf("%d",n2);
+    return 0;
+}
